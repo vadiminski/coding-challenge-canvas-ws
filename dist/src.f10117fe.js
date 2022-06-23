c7863f134841baae87f08ec90b0c036a814fdfd3
vadim <vadim@vadimsPC.local>
2022-06-23 18:29:41 +0200

    added nodemon as dev dependency

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -6635,7 +6635,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46397" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44701" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
